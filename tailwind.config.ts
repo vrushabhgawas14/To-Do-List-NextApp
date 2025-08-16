9e3c876f2b8b14e7707e1a5ffd9b94a6af09d6b6
Vrushabh <vrushabhgawas14@gmail.com>
2025-08-16 13:42:51 +0530

    Initial Commit

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: { max: "900px" },
+        md: { min: "901px", max: "1024px" },
+        lg: "1025px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
